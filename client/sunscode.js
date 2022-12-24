0f9832070bb1f8b20eb07e5f30e2f739448a9337
SuNiL.Chau <1234huts@gmail.com>
2022-12-24 16:34:41 +0530

    updated path

--- client/sunscode.js
+++ client/sunscode.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
   loader(msgDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://sunscode.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
